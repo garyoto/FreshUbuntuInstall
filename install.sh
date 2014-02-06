64916088f0bb0fbdc5e9a5af46a249cfe88a047f
Garret <garretlewis@gmail.com>
2014-02-06 02:15:59 -0600

    still working out version kinks...
    pgrouting does not like using postgresql 9.3

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-add-repository -y multiverse
+sudo apt-get -y install pgadmin
 sudo apt-get -y install aptitude
 sudo apt-get -y install docky
 sudo apt-get -y install chromium-browser
@@ -36,14 +37,27 @@ sudo pip install virtualenvwrapper
 sudo pip install ipython-cluster-helper
 sudo pip install ipython-sql               #RDBMS access via ipython
 
+# Add Ivan Mincik repository for latest QGIS
+sudo add-apt-repository ppa:imincik/qgis-master
+sudo apt-get update
+sudo apt-get -y install qgis=2.1.0+git20131214~c4b2480~precise1  #latest as of 2/6/2014
+
 # Add Ubuntu-GIS repository
 sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
-sudo apt-get -y install qgis
+sudo apt-get -y install grass libgdal-ecw-src libgdal-grass libgeotiff-dfsg liblas spatialite spatialite-gui spatialite-tools saga
 
-# Add cartodb repository
-sudo add-apt-repository -y ppa:cartodb/gis #There are overlaps of packages in Cartodb and UbuntuGIS. Becareful!
-sudo add-apt-repository -y ppa:cartodb/postgresql-9.3
+# Add cartodb repositories
+sudo add-apt-repository -y ppa:cartodb/postgresql-9.3 #There are overlaps of packages in Cartodb and UbuntuGIS. Its good to specify a version number. Becareful!
+sudo add-apt-repository -y ppa:cartodb/gis
+sudo apt-get update
+sudo apt-get install postgresql-9.3-postgis-2.1=2.1.0-4.cdb+1precise4 postgresql-9.3-postgis-2.1-scripts=2.1.0-4.cdb+1precise4  #as of 2/6/2014 version postgis 2.1 with postgresql 9.3
+sudo apt-get -y install gdal-bin=1.10.1-0cdb1~precise2 libgdal-dev
+
+#Add Team Georepublic repository for pgrouting
+sudo add-apt-repository ppa:georepublic/pgrouting
+sudo apt-get update
+sudo apt-get -y install postgresql-9.1-pgrouting osm2pgrouting pgrouting-workshop
 sudo apt-get update
 
 #install phonegap and denpendencies
